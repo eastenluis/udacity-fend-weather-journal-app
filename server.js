e1d9a7e32ce219a737169ffefd052e751e51365a
Martin Lai <eastenluis@hotmail.com>
2020-10-24 17:32:24 -0400

    update api codes

--- server.js
+++ server.js
@@ -1,19 +1,50 @@
 // Setup empty JS object to act as endpoint for all routes
-projectData = {};
+const projectData = {
+    journals: [],
+};
 
 // Require Express to run server and routes
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
 // Start up an instance of app
+const app = express();
 
-/* Middleware*/
-//Here we are configuring express to use body-parser as middle-ware.
+/* Middleware */
+// Here we are configuring express to use body-parser as middle-ware.
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
+app.use(cors());
+
+// Weather journal routes
+app.get('api/journals', (req, res) => {
+    // Return a list of recent searched journals, as JSON array
+    res.send(projectData.journals);
+});
+
+const requiredFields = ['temp', 'date', 'userResponse'];
+app.post('api/journals', (req, res) => {
+    for (const field of requiredFields) {
+        if (!req.body || !req.body[field]) {
+            res.status(400).send(`Missing required field: "${field}"`);
+            return;
+        }
+    }
+    const { temp, date, userResponse } = req.body;
+    projectData.journals.push({
+        temp, date, userResponse,
+    });
+    res.status('201').end();
+});
 
 // Initialize the main project folder
 app.use(express.static('website'));
 
-
 // Setup Server
+const port = 8000;
+app.listen(port, () => {
+    console.log(`Server running on localhost:${port}`);
+});
